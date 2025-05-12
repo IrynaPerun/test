c1881b275848687b8f9f7a588c180617b3e00e05
Iryna Sabatovych-Perun <iryna.sabatovych.perun@gmail.com>
2025-05-12 21:15:01 +0300

    clean up AddWorkComponent component

--- src/app/features/works-list/interface/work-item.interface.ts
+++ src/app/features/works-list/interface/work-item.interface.ts
@@ -2,5 +2,17 @@ export interface IWorkItem {
   id: number;
   name: string;
   points: number;
-  repeat: number;
+  repeat: number[];
+  days?: IDayOfTheWeek[];
+}
+
+export interface IDaysOfTheWeek {
+  name: string;
+  completed: boolean;
+  days?: IDayOfTheWeek[];
+}
+
+export interface IDayOfTheWeek {
+  name: string;
+  completed: boolean;
 }
--- src/app/features/works-list/modals/add-work/add-work.component.ts
+++ src/app/features/works-list/modals/add-work/add-work.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import {Component, computed, inject, signal} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialog,
@@ -8,12 +8,13 @@ import {
   MatDialogRef,
   MatDialogTitle
 } from "@angular/material/dialog";
-import {MatButton} from "@angular/material/button";
-import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatOption, MatSelect} from "@angular/material/select";
-import {IWorkItem} from "../../interface/work-item.interface";
+import { MatButton } from "@angular/material/button";
+import { MatError, MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatOption, MatSelect } from "@angular/material/select";
+import { IDaysOfTheWeek, IWorkItem } from "../../interface/work-item.interface";
+import {MatCheckbox} from "@angular/material/checkbox";
 
 @Component({
   selector: 'app-add-work',
@@ -30,7 +31,8 @@ import {IWorkItem} from "../../interface/work-item.interface";
     MatLabel,
     ReactiveFormsModule,
     MatSelect,
-    MatOption
+    MatOption,
+    MatCheckbox
   ],
   templateUrl: './add-work.component.html',
   styleUrl: './add-work.component.css'
@@ -42,7 +44,9 @@ export class AddWorkComponent {
     id: new FormControl(this.dialogData?.id || null),
     name: new FormControl(this.dialogData?.name || '', [Validators.required, Validators.minLength(6)]),
     points: new FormControl(this.dialogData?.points || '50', [Validators.required, Validators.min(10)]),
-    repeat: new FormControl(this.dialogData?.repeat || '0', [Validators.required,]),
+    monday: new FormControl(this.dialogData?.days?[0]?['completed'] || false),
+    thursday: new FormControl(this.dialogData?.days?[0]?['completed'] || false),
+    
   });
 
   submit () {
@@ -52,5 +56,41 @@ export class AddWorkComponent {
     }
     console.log(this.form.value);
     this.dialog.close(this.form.value);
+  }
+
+  //daysOfTheWeek: IDaysOfTheWeek = inject(MAT_DIALOG_DATA);
+  readonly daysOfTheWeek = signal<IDaysOfTheWeek>({
+    name: 'Days of the Week',
+    completed: false,
+    days: [
+      {name: 'Monday', completed: false},
+      {name: 'Tuesday', completed: false},
+      {name: 'Wednesday', completed: false},
+      {name: 'Thursday', completed: false},
+      {name: 'Friday', completed: false},
+      {name: 'Saturday', completed: false},
+      {name: 'Sunday', completed: false},
+    ],
+  });
+
+  readonly partiallyComplete = computed(() => {
+    const daysOfTheWeek = this.daysOfTheWeek();
+    if (!daysOfTheWeek.days) {
+      return false;
+    }
+    return daysOfTheWeek.days.some(d => d.completed) && !daysOfTheWeek.days.every(d => d.completed);
+  });
+
+  update(completed: boolean, index?: number) {
+    this.daysOfTheWeek.update(daysOfTheWeek => {
+      if (index === undefined) {
+        daysOfTheWeek.completed = completed;
+        daysOfTheWeek.days?.forEach(d => (d.completed = completed));
+      } else {
+        daysOfTheWeek.days![index].completed = completed;
+        daysOfTheWeek.completed = daysOfTheWeek.days?.every(d => d.completed) ?? true;
+      }
+      return {...daysOfTheWeek};
+    });
   }
 }
